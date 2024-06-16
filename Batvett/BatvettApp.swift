6093c53790b534790e20018be947623f8c074b06
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2024-06-16 19:43:19 +0200

    Initial Commit

---
+++ Batvett/BatvettApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatvettApp.swift
+//  Batvett
+//
+//  Created by Sondre Stokkan Spæren on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BatvettApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
